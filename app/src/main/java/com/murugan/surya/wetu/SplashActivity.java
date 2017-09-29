e705c0c7335d968d33b3c98a08da0a77368b8c0b
suryamurugan <surya3542live@gmail.com>
2017-09-30 01:43:36 +0530

    V6 - Firebase GOOGLE AUTH ADDED+UI IMPROVEMENTS+ADS+SEXY !!

--- app/src/main/java/com/murugan/surya/wetu/SplashActivity.java
+++ app/src/main/java/com/murugan/surya/wetu/SplashActivity.java
@@ -16,7 +16,7 @@ public class SplashActivity extends AppCompatActivity {
 
 
 
-                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                Intent intent = new Intent(SplashActivity.this, googleAuth.class);
 
                 SplashActivity.this.startActivity(intent);
         finish();
